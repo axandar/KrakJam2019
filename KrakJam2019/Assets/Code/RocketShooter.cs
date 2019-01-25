ea38b91eb80a5dea64b985c757da24fb62cd32d7
Szymon Przybyszewski <szymon.przybyszewski94@gmail.com>
2019-01-25 20:10:51 +0100

    Shooting Prototype

---
+++ KrakJam2019/Assets/Code/RocketShooter.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketShooter : MonoBehaviour {
+
+     [SerializeField] GameObject rocketLauncher;
+     [SerializeField] GameObject rocket;
+     [SerializeField] float rocketVelocity;
+     [SerializeField] float rocketTimer;
+     [SerializeField] float shootingSpeed;
+
+     bool enableShooting = true;
+     
+     void Update() {
+          ShootRocket();
+     }
+     
+     void ShootRocket() {
+          if (enableShooting) {
+               if (Input.GetMouseButton(0)) {
+                    enableShooting = false;
+                    var temporaryRocket = Instantiate(rocket, rocketLauncher.transform.position,
+                         rocketLauncher.transform.rotation) as GameObject;
+
+                    var temporaryRigidbody = temporaryRocket.GetComponent<Rigidbody>();
+                    temporaryRigidbody.AddForce(transform.up * rocketVelocity);
+                    Invoke(nameof(EnableShooting), shootingSpeed);
+                    Destroy(temporaryRocket, rocketTimer);
+               }
+          }
+     }
+
+     void EnableShooting() {
+          enableShooting = true;
+     }
+}
