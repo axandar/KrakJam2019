6c56f2c692592a59835143fbb1f7e281a357088a
Szymon Przybyszewski <szymon.przybyszewski94@gmail.com>
2019-01-25 21:09:34 +0100

    Scene reloader after gameover

--- KrakJam2019/Assets/Code/GameController.cs
+++ KrakJam2019/Assets/Code/GameController.cs
@@ -2,14 +2,37 @@
 using System.Collections.Generic;
 using System.Net.Mime;
 using UnityEngine;
+using UnityEngine.Analytics;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GameController : MonoBehaviour {
 
     [SerializeField] Text scoreText;
+    [SerializeField] Text healthText;
+    [SerializeField] GameObject gameUI;
+    [SerializeField] GameObject gameOverUI;
+    
     int scoreValue;
+    [SerializeField] int healthValue;
 
+    void Start() {
+        gameUI.SetActive(true);
+        gameOverUI.SetActive(false);
+    }
+    
     void Update() {
         scoreText.text = "Score: " + scoreValue;
+        healthText.text = "Health: " + healthValue;
+
+        if (healthValue <= 0)
+            GameOver();
+    }
+
+    void GameOver() {
+        gameUI.SetActive(false);
+        gameOverUI.SetActive(true);
+        if (Input.GetKeyDown(KeyCode.R))  
+            SceneManager.LoadScene( SceneManager.GetActiveScene().buildIndex ) ;
     }
 }
