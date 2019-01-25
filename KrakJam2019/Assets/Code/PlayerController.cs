6812bf51f6382dd8b62034e41c9fd7e5e47b3b19
Szymon Przybyszewski <szymon.przybyszewski94@gmail.com>
2019-01-25 23:21:09 +0100

    Cleanup

--- KrakJam2019/Assets/Code/PlayerController.cs
+++ KrakJam2019/Assets/Code/PlayerController.cs
@@ -4,23 +4,22 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+   Rigidbody2D _rb;
+   float _xAxis;
+   float _yAxis;
 
-   private Rigidbody2D _rb;
-   private float _xAxis;
-   private float _yAxis;
 
+   [SerializeField] float timeToStop = 0.01f;
+   [SerializeField] float _maxVelocity = 3;
+   [SerializeField] float _rotationSpeed = 2;
 
-   [SerializeField] private float timeToStop = 0.01f;
-   [SerializeField] private float _maxVelocity = 3;
-   [SerializeField] private float _rotationSpeed = 2;
-
-   private void Start()
+   void Start()
    {
       _rb = GetComponent<Rigidbody2D>();
       
    }
 
-   private void Update()
+   void Update()
    {
       _xAxis = Input.GetAxis("Vertical");
       _yAxis = Input.GetAxis("Horizontal");
@@ -28,7 +27,7 @@ public class PlayerController : MonoBehaviour
    }
 
 
-   private void GetInput()
+   void GetInput()
    {
    
       if(Input.GetKey(KeyCode.W)){        
@@ -51,20 +50,21 @@ public class PlayerController : MonoBehaviour
      // ClampVelocity();
    }
 
-   private void ClampVelocity()
+   void ClampVelocity()
    {
       float x = Mathf.Clamp(_rb.velocity.x, -_maxVelocity , _maxVelocity);
       float y = Mathf.Clamp(_rb.velocity.y, -_maxVelocity , _maxVelocity);
    
       _rb.velocity = new Vector2(x,y);
    }
-   private void MoveForvard(float amount)
+
+   void MoveForvard(float amount)
    {
       Vector2 force = transform.up * amount;
       _rb.AddForce(force); 
    }
 
-   private void Rotate(Transform t, float amount)
+   void Rotate(Transform t, float amount)
    {
       t.Rotate(0, 0, amount);
    }
