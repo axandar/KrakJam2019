e624b9b2e9bdb1a0a116ea9f621519e7b81b6e32
Michał Piątkowski <axandar29@gmail.com>
2019-01-25 23:54:50 +0100

    Added enemy spawner and basic enemy behaviour

---
+++ KrakJam2019/Assets/Code/EnemyMovements/LineToPlayer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Code.EnemyMovements{
+	[Serializable]
+	public class LineToPlayer : EnemyMovement{
+
+		[SerializeField] private int predictionStepMillis = 100;
+		[SerializeField] private int predictionTimeMillis = 4000;
+		private readonly Queue<Vector3> _route = new Queue<Vector3>();
+		
+		public override void GenerateRoute(Vector3 start, Vector3 target){
+			var heading = target - start;
+			var distance = heading.magnitude;
+			
+			var step = distance / (int)(predictionTimeMillis / predictionStepMillis);
+			for(var i = predictionStepMillis; i <= predictionTimeMillis; i += predictionStepMillis){
+				var vec3 = Vector3.MoveTowards(start, target, step);
+				start = vec3;
+				_route.Enqueue(vec3);
+			}
+		}
+
+		public override bool IsNextVector(){
+			return _route.Count > 0;
+		}
+
+		public override Vector3 GetNextVector(){
+			return _route.Dequeue();
+		}
+	}
+}
---
+++ KrakJam2019/Assets/Code/RespawnArea.cs
@@ -0,0 +1,41 @@
+using System;
+using UnityEngine;
+
+namespace Code{
+	[Serializable]
+	public class RespawnArea{
+		[SerializeField] private float minX;
+		[SerializeField] private float maxX;
+		[SerializeField] private float maxY;
+		[SerializeField] private float minY;
+
+		public RespawnArea(float minX, float maxX, float maxY, float minY){
+			this.minX = minX;
+			this.maxX = maxX;
+			this.maxY = maxY;
+			this.minY = minY;
+		}
+
+		
+		
+		public float MinX{
+			get => minX;
+			set => minX = value;
+		}
+
+		public float MaxX{
+			get => maxX;
+			set => maxX = value;
+		}
+
+		public float MaxY{
+			get => maxY;
+			set => maxY = value;
+		}
+
+		public float MinY{
+			get => minY;
+			set => minY = value;
+		}
+	}
+}
