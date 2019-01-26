6b43c773d27ea80a2c4ce08104d9cfc11b923024
Daniel Marcinkowski <overgrape@gmail.com>
2019-01-26 07:11:51 +0100

    Random Ammo

--- KrakJam2019/Assets/Code/RocketShooter.cs
+++ KrakJam2019/Assets/Code/RocketShooter.cs
@@ -6,14 +6,28 @@ using UnityEngine;
 public class RocketShooter : MonoBehaviour {
 
      [SerializeField] GameObject rocketLauncher;
+     [SerializeField] GameObject rocket01;
+     [SerializeField] GameObject rocket02;
+     [SerializeField] GameObject rocket03;
      [SerializeField] GameObject rocket;
+     
+
      [SerializeField] float rocketVelocity;
      [SerializeField] float rocketTimer;
      [SerializeField] float shootingDelay;
+     private int randomValues = 2;
+     private static int bulletId;
+
     
 
      bool enableShooting = true;
-     
+
+     private void Awake()
+     {
+          StartCoroutine(Kappa());
+          rocket = rocket01;
+     }
+
      void Update() {
           ShootRocket();
      }
@@ -37,6 +51,28 @@ public class RocketShooter : MonoBehaviour {
      void EnableShooting() {
           enableShooting = true;
      }
+    
+     private IEnumerator Kappa()
+     {
+          while (true) {
+               randomValues = Random.Range(0, 3);
+               bulletId = Random.Range(0, 4);
+               Debug.Log(bulletId);
+               yield return new WaitForSeconds(randomValues);
 
+               switch (bulletId) {
+                         case 1:
+                              rocket = rocket01;
+                              break;
+                         case 2:
+                              rocket = rocket02;
+                              break;
+                         case 3:
+                              rocket = rocket03;
+                              break;
+               }
+          }
+          
+     }
     
 }
