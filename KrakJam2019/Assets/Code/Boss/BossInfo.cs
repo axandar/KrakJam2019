90958a8ee91c5ca227c6f1b2ad0507b44a72444e
Cainie <karol.marszalik@gmail.com>
2019-01-26 15:02:45 +0100

    BossAI, BossRespawn, BossInfo, BossPrefab

---
+++ KrakJam2019/Assets/Code/Boss/BossInfo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Code.Enemy;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class BossInfo : MonoBehaviour{
+   
+   private float currentHealth;
+
+   [SerializeField] private float _health;
+   [SerializeField] private int _scoreValue;
+   [SerializeField] private BossRespawner _bossRespawner;
+   
+   
+   public AddScoreEvent addScore;
+
+
+   private void OnEnable()
+   {
+      CurrentHealth = _health;
+   }
+
+   public float CurrentHealth{
+      get { return currentHealth; }
+      set{
+         currentHealth = value;
+         if (currentHealth <= 0){
+            _bossRespawner.isAlive = false;
+            addScore.Invoke(_scoreValue);
+            gameObject.SetActive(false);
+         }
+      }
+   }
+   
+   [Serializable]
+   public class AddScoreEvent : UnityEvent<int> {
+   }
+   
+  
+}
