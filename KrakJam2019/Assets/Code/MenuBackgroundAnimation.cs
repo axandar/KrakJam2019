c746efb9fc30f7ccc92850148e8035f5bd8e0574
Szymon Przybyszewski <szymon.przybyszewski94@gmail.com>
2019-01-26 02:11:56 +0100

    Menu scene init

---
+++ KrakJam2019/Assets/Code/MenuBackgroundAnimation.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuBackgroundAnimation : MonoBehaviour {
+
+    [SerializeField] float newXvalue, newYvalue;
+    
+    void Update () {
+        transform.position = new Vector3(transform.position.x + newXvalue, transform.position.y + newYvalue, 0);
+    }
+}
