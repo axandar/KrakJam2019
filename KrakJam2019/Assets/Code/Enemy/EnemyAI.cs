ededea6c79643f0e8139d4b4ad36a3df74f57387
Szymon Przybyszewski <szymon.przybyszewski94@gmail.com>
2019-01-26 12:00:38 +0100

    BOOM BOOM MANAGER DZIALA! (popsul sie Score Kappa)

--- KrakJam2019/Assets/Code/Enemy/EnemyAI.cs
+++ KrakJam2019/Assets/Code/Enemy/EnemyAI.cs
@@ -2,16 +2,16 @@
 using UnityEngine;
 using UnityEngine.Events;
 
-namespace Code.Enemy{
+namespace Code.Enemy {
 	// ReSharper disable once InconsistentNaming
-	public class EnemyAI : MonoBehaviour{
+	public class EnemyAI : MonoBehaviour {
 		public Transform target;
 
-		[SerializeField] private EnemyMovement enemyMovement;
-		[SerializeField] private Vector3 nextStep;
-		[SerializeField] private float speed = 5;
-		[SerializeField] private bool isBomb;
-		[SerializeField] private int scoreValue;
+		[SerializeField] EnemyMovement enemyMovement;
+		[SerializeField] Vector3 nextStep;
+		[SerializeField] float speed = 5;
+		[SerializeField] bool isBomb;
+		[SerializeField] int scoreValue;
 		public AddScoreEvent addScore;
 		public float health = 5;
 		float currentHealth;
@@ -29,46 +29,67 @@ namespace Code.Enemy{
 			nextStep = enemyMovement.GetNextVector();
 		}
 
-		void Update(){
-			if(!isBomb){
+		void Update() {
+			if (!isBomb) {
 				//przeciwnicy odwracaja sie w strone gracza
 			}
 
-			if(health <= 0){
+			if (currentHealth <= 0) {
 				addScore.Invoke(scoreValue);
-			if(currentHealth <= 0){
+				if (currentHealth <= 0) {
+					Destroy(gameObject);
+				}
+			}
+		}
+
+		void FixedUpdate(){
+			if(_isMoving){
+				MoveEnemy();
+			} else{
+				if(isBomb){
+					Debug.Log("BOOM");
+				}
+
 				Destroy(gameObject);
 			}
 		}
 
-		void MoveEnemy(){
-			if(IsInNextStep()){
-				if(enemyMovement.IsNextVector()){
-					nextStep = enemyMovement.GetNextVector();
-				} else{
-					_isMoving = false;
+		void MoveEnemy() {
+				if (IsInNextStep()) {
+					if (enemyMovement.IsNextVector()) {
+						nextStep = enemyMovement.GetNextVector();
+					}
+					else {
+						_isMoving = false;
+					}
 				}
+
+				var step = speed * Time.deltaTime;
+				transform.position = Vector3.MoveTowards(transform.position, nextStep, step);
 			}
 
-			var step = speed * Time.deltaTime;
-			transform.position = Vector3.MoveTowards(transform.position, nextStep, step);
+			bool IsInNextStep() {
+				var distance = Vector3.Distance(transform.position, nextStep);
+				return distance < 0.01f;
+			}
+		
+		public void DamageMeBoi(int boomBoomValue) {
+			currentHealth -= boomBoomValue;
 		}
 
-		bool IsInNextStep(){
-			var distance = Vector3.Distance(transform.position, nextStep);
-			return distance < 0.01f;
-		}
-
-			RespawnArea GetRespawnArea(){
+		public RespawnArea GetRespawnArea() {
 			return enemyMovement.GetRespawnArea();
 		}
-
-
-			void DamageMeBoi(int damage) {
-			currentHealth -= damage;
+		
+		[ContextMenu("Kill")]
+		private void Kill(){
+			health -= health;
 		}
+		
+		[Serializable]
+		public class AddScoreEvent : UnityEvent<int> {
+		}
+
+
 	}
-	
-	[Serializable]
-	public class AddScoreEvent : UnityEvent<int>{}
 }
