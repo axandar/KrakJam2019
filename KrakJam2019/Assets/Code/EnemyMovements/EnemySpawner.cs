837d74fc3d915fa0d3eb930cabe3e9a7f28aafb7
Szymon Przybyszewski <szymon.przybyszewski94@gmail.com>
2019-01-26 04:11:46 +0100

    more code cleaup

--- KrakJam2019/Assets/Code/EnemyMovements/EnemySpawner.cs
+++ KrakJam2019/Assets/Code/EnemyMovements/EnemySpawner.cs
@@ -3,44 +3,44 @@ using UnityEngine;
 
 namespace Code{
 	public class EnemySpawner : MonoBehaviour{
-		[SerializeField] private Camera gameCamera;
+		[SerializeField] Camera gameCamera;
 
-		[SerializeField] private GameObject enemyBombPrefab;
-		[SerializeField] private GameObject enemyLineXRightPrefab;
-		[SerializeField] private GameObject enemyLineXLeftPrefab;
+		[SerializeField] GameObject enemyBombPrefab;
+		[SerializeField] GameObject enemyLineXRightPrefab;
+		[SerializeField] GameObject enemyLineXLeftPrefab;
 		
-		[SerializeField] private Transform playerTransform;
-		[SerializeField] private Transform wallLeftTransform;
-		[SerializeField] private Transform wallRightTransform;
+		[SerializeField] Transform playerTransform;
+		[SerializeField] Transform wallLeftTransform;
+		[SerializeField] Transform wallRightTransform;
 
-		private void Start(){
+		void Start(){
 			SpawnEnemyGoesInLineXRight();
 			SpawnEnemyGoesInLineXLeft();
 			SpawnEnemyBomb();
 		}
 
-		private void SpawnEnemyGoesInLineXRight(){
+		void SpawnEnemyGoesInLineXRight(){
 			var enemy = Instantiate(enemyLineXRightPrefab);
 			var enemyAi = enemy.GetComponent<EnemyAI>();
 			enemyAi.target = wallRightTransform;
 			enemy.transform.position = GenerateVectorForSpawn(enemyAi.GetRespawnArea());
 		}
 
-		private void SpawnEnemyGoesInLineXLeft(){
+		void SpawnEnemyGoesInLineXLeft(){
 			var enemy = Instantiate(enemyLineXLeftPrefab);
 			var enemyAi = enemy.GetComponent<EnemyAI>();
 			enemyAi.target = wallLeftTransform;
 			enemy.transform.position = GenerateVectorForSpawn(enemyAi.GetRespawnArea());
 		}
 
-		private void SpawnEnemyBomb(){
+		void SpawnEnemyBomb(){
 			var enemy = Instantiate(enemyBombPrefab);
 			var enemyAi = enemy.GetComponent<EnemyAI>();
 			enemyAi.target = playerTransform;
 			enemy.transform.position = GenerateVectorForSpawn(enemyAi.GetRespawnArea());
 		}
 
-		private Vector3 GenerateVectorForSpawn(RespawnArea respawnArea){
+		Vector3 GenerateVectorForSpawn(RespawnArea respawnArea){
 			var x = Random.Range(respawnArea.MinX, respawnArea.MaxX);
 			var y = Random.Range(respawnArea.MinY, respawnArea.MaxY);
 			return gameCamera.ViewportToWorldPoint(new Vector3(x, y, 1));
