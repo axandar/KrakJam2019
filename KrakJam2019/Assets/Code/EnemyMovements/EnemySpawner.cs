3fc6df1190e4e51652991a15181f20c7189f3a86
Michał Piątkowski <axandar29@gmail.com>
2019-01-27 02:00:49 +0100

    Added configurable values for enemy spawner

--- KrakJam2019/Assets/Code/EnemyMovements/EnemySpawner.cs
+++ KrakJam2019/Assets/Code/EnemyMovements/EnemySpawner.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Code.Enemy;
 using UnityEngine;
 
@@ -14,6 +15,16 @@ namespace Code.EnemyMovements{
 		[SerializeField] private Transform playerTransform;
 		[SerializeField] private Transform wallLeftTransform;
 		[SerializeField] private Transform wallRightTransform;
+
+		[Header("Spawner values")] 
+		[SerializeField] private float inLineXRightSpawnChance = 33.33f;
+		[SerializeField] private float inLineXLeftSpawnChance = 33.33f;
+		[SerializeField] private float bombSpawnChance = 33.33f;
+		[SerializeField] private int minIntervalInMillis = 100;
+		[SerializeField] private int maxIntervalInMillis = 1000;
+		[SerializeField] private int minNumberOfSpawnedAtOnce = 1;
+		[SerializeField] private int maxNumberOfSpawnedAtOnce = 10;
+		[SerializeField] private int timeFlowMultiplicatorBetweenIntervals = 350;
 
 		private readonly Queue<EnemySpawnInfo> _enemySpawnInfoQueue = new Queue<EnemySpawnInfo>();
 
@@ -33,7 +44,7 @@ namespace Code.EnemyMovements{
 			}
 
 			var spawnInfo = _enemySpawnInfoQueue.Peek();
-			var temp = (int) (Time.deltaTime * 350);
+			var temp = (int) (Time.deltaTime * timeFlowMultiplicatorBetweenIntervals);
 			spawnInfo.IntervalInMillis -= temp;
 			if(spawnInfo.IntervalInMillis <= 0){
 				spawnInfo.SpawnEnemy.Invoke();
@@ -42,17 +53,19 @@ namespace Code.EnemyMovements{
 		}
 
 		private void GenerateSpawnInfo(){
-			var quantity = Random.Range(1, 10);
+			var quantity = Random.Range(minNumberOfSpawnedAtOnce, maxNumberOfSpawnedAtOnce);
 			for(var i = 0; i < quantity; i++){
 				var spawnFunction = GetSpawnEnemyFunction();
-				var interval = Random.Range(100, 1000);
+				var interval = Random.Range(minIntervalInMillis, maxIntervalInMillis);
 				var spawnInfo = new EnemySpawnInfo(interval, spawnFunction);
 				_enemySpawnInfoQueue.Enqueue(spawnInfo);
 			}
 		}
 
 		private EnemySpawnInfo.SpawnEnemyFunction GetSpawnEnemyFunction(){
-			var index = Random.Range(0, 3);
+			var weightsList = new List<float>{inLineXRightSpawnChance, inLineXLeftSpawnChance, bombSpawnChance};
+			var index = GetRandomWeightedIndex(weightsList);
+			
 			switch(index){
 				case 0:
 					return _inLineXRight;
@@ -63,6 +76,24 @@ namespace Code.EnemyMovements{
 				default:
 					return _bomb;
 			}
+		}
+
+		private static int GetRandomWeightedIndex(IReadOnlyList<float> weights){
+			var weightSum = weights.Sum();
+			
+			var index = 0;
+			var lastIndex = weights.Count - 1;
+			while(index < lastIndex){
+				// Do a probability check with a likelihood of weights[index] / weightSum.
+				if(Random.Range(0, weightSum) < weights[index]){
+					return index;
+				}
+
+				// Remove the last item from the sum of total untested weights and try again.
+				weightSum -= weights[index++];
+			}
+			
+			return index;
 		}
 
 		private void SpawnEnemyGoesInLineXRight(){
